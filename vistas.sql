926924354ceb94cf07285c1d82a63188535e9b29
JeiData <99595411+JeiData@users.noreply.github.com>
2022-02-28 22:46:50 -0300

    Update vistas.sql

--- vistas.sql
+++ vistas.sql
@@ -1,19 +1,44 @@
+CREATE VIEW Proyecto_disciplina
+AS SELECT
+	Proyecto_disciplina.proyecto_id,
+	Proyecto_disciplina.disciplina_id,
+	ref_disciplina.Disciplina_Id,
+    ref_disciplina.Area_descripcion,
+    ref_disciplina.Disciplina_descripcion
+FROM
+    Proyecto_disciplina, 
+    ref_disciplina
+WHERE
+    Proyecto_disciplina.disciplina_id=ref_disciplina.Disciplina_Id;
+
+------------
+
 CREATE VIEW Vista_integrantes
 AS SELECT * FROM integrantes;
 
+---------------
 CREATE OR REPLACE VIEW Vista_integrantes_femeninos
 AS SELECT Proyecto_Id, Cantidad_Miembros_F 
 FROM integrantes
 ORDER BY Cantidad_Miembros_F DESC ;
 
+----------------
+
 CREATE VIEW vista_financiamiento
 AS SELECT
-	   financiamiento.proyecto_id,
-     financiamiento.Moneda_id,
+	 financiamiento.Proyecto_Id,
      financiamiento.Monto_Adjudicado,
-     ref_moneda.Moneda_id
+     ref_moneda.Moneda_Id,
+     ref_moneda.Moneda
 FROM
-    financiamiento
+    financiamiento, 
     ref_moneda
 WHERE
-    financiamiento.moneda_id = ref_moneda.moneda_id;
+    financiamiento.moneda_id=ref_moneda.Moneda;
+
+------------
+
+CREATE OR REPLACE VIEW Prioridades_inversion
+AS SELECT Proyecto_Id, Monto_Adjudicado
+FROM financiamiento
+ORDER BY Monto_Adjudicado DESC ;
