2d9b02e9a8b7141c45aebaf7d482d2a8ed90e79b
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-02 19:35:58 -0300

    Create funciones.sql

---
+++ funciones.sql
@@ -0,0 +1,14 @@
+SELECT
+AVG (Monto_Adjudicado)
+FROM financiamiento;
+
+CREATE FUNCTION `porcentaje_miembros_mujeres`(param1 INT , param2 INT) RETURNS int(11)
+NO SQL
+BEGIN
+	DECLARE resultado INT;
+  DECLARE total_miembros INT;
+	SET total_miembros=(param1 + param2);
+  SET resultado = (param1 /total_miembros *100);
+	RETURN resultado;
+END
+
