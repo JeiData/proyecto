5c1d9c5320ae8dfb03c84edf4b5074afea478a45
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-01 21:19:38 -0300

    Update vistas.sql

--- vistas.sql
+++ vistas.sql
@@ -3,20 +3,49 @@ AS SELECT
 	Proyecto_disciplina.proyecto_id,
 	Proyecto_disciplina.disciplina_id,
 	ref_disciplina.Disciplina_Id,
-    ref_disciplina.Area_descripcion,
-    ref_disciplina.Disciplina_descripcion
+    	ref_disciplina.Area_descripcion,
+   	ref_disciplina.Disciplina_descripcion
 FROM
-    Proyecto_disciplina, 
-    ref_disciplina
+	Proyecto_disciplina, 
+    	ref_disciplina
 WHERE
-    Proyecto_disciplina.disciplina_id=ref_disciplina.Disciplina_Id;
+    	Proyecto_disciplina.disciplina_id=ref_disciplina.Disciplina_Id;
 
-------------
+------------ CORRECCION EN BASE A LO SUGERIDO:
+CREATE VIEW Vista_Proyecto_disciplina
+AS SELECT
+	Proyecto_disciplina.proyecto_id,
+	Proyecto_disciplina.disciplina_id as disc_id,
+	ref_disciplina.Disciplina_Id as disc,
+    	ref_disciplina.Area_descripcion,
+   	ref_disciplina.Disciplina_descripcion
+FROM
+	Proyecto_disciplina, 
+    	ref_disciplina
+WHERE
+    	Proyecto_disciplina.disc_id=ref_disciplina.dis;
+
+
+------------ OTRA OPCION PARA LA MISMA IDEA:
+
+CREATE VIEW Vista_proyecto_disciplina
+AS SELECT
+	proyecto_id, disciplina_id
+FROM
+	proyecto_disciplina
+UNION ALL
+SELECT
+	Area_descripcion, Disciplina_descripcion
+FROM
+	ref_disciplina;
+
+-----------------------
+
 
 CREATE VIEW Vista_integrantes
 AS SELECT * FROM integrantes;
 
----------------
+----------------------
 CREATE OR REPLACE VIEW Vista_integrantes_femeninos
 AS SELECT Proyecto_Id, Cantidad_Miembros_F 
 FROM integrantes
