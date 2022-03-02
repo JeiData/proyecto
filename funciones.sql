5636f6744e4df839cec9943afcfa662980c82ba1
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-02 19:50:30 -0300

    Update funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -6,9 +6,9 @@ CREATE FUNCTION `porcentaje_miembros_mujeres`(param1 INT , param2 INT) RETURNS i
 NO SQL
 BEGIN
 	DECLARE resultado INT;
-  DECLARE total_miembros INT;
+ 	DECLARE total_miembros INT;
 	SET total_miembros=(param1 + param2);
-  SET resultado = (param1 /total_miembros *100);
+	SET resultado = (param1 /total_miembros *100);
 	RETURN resultado;
 END
 
