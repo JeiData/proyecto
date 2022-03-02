5828c8c0fd030e9ca05cf0efe139ae9e3a4859c7
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-02 20:15:38 -0300

    Update funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -14,3 +14,13 @@ BEGIN
 	RETURN resultado;
 END
 
+-----------------
+
+CREATE FUNCTION `FINANCIAMIENTO`(MONEDA_id INT) RETURNS varchar(30) READS SQL 
+DATA 
+BEGIN 
+	DECLARE resultado VARCHAR (30);
+	SET resultado = (SELECT Monto_Solicitado FROM proyectos_ciencia.financiamiento WHERE proyectos_ciencia.financiamiento.moneda_id = moneda_id);
+	RETURN resultado;
+END
+
