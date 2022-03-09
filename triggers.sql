835ee5ab7ad8649414816e836d47451bb74054b9
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-09 20:35:03 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -32,3 +32,39 @@ INSERT INTO Proyectos_2019
 VALUES
 (24655,'CONICET', 'SALUD MENTAL Y REDES SOCIALES', 'Este proyecto pretende investigar el vinculo entre el denominado burnout y el uso adictivo de redes sociales', '222019635478CO', 'Redes sociales. Psicologia.');
   
+
+-- TRIGGER NRO 2 (BEFORE)
+-- Permitira llevar un control de la insercion de nuevos registros en la tabla .
+
+-- 1 Creacion de la tabla de auditoria
+CREATE TABLE Aud_Financiamiento (
+Id_movimiento INT NOT NULL AUTO_INCREMENT,
+Fecha DATETIME NOT NULL,
+Usuario_Id VARCHAR (30) NOT NULL,
+Proyecto_Id INT NOT NULL,                   -- DE LA TABLA FINANCIAMIENTO
+Moneda_Id INT NOT NULL,                     -- DE LA TABLA FINANCIAMIENTO
+Monto_Solicitado INT NOT NULL,              -- DE LA TABLA FINANCIAMIENTO
+Monto_Adjudicado INT NOT NULL,              -- DE LA TABLA FINANCIAMIENTO
+Monto_Financiado_solicitado INT NOT NULL,   -- DE LA TABLA FINANCIAMIENTO
+Monto_Financiado_adjudicado INT NOT NULL,   -- DE LA TABLA FINANCIAMIENTO
+Tipo_Operacion VARCHAR (200) NULL,
+PRIMARY KEY (Id_movimiento)
+);
+
+-- 2 Creacion del Trigger After
+DROP TRIGGER IF EXISTS `insp_financiamiento`;
+
+CREATE TRIGGER `aud_financiamiento`
+BEFORE INSERT ON Financiamiento -- EVENTO QUE DESENCADENARIA EL TRIGGER
+FOR EACH ROW
+INSERT INTO aud_financiamiento
+(Id_movimiento,Fecha,Usuario_Id,Proyecto_Id,Moneda_Id,Monto_Solicitado,Monto_Adjudicado,Monto_Financiado_Solicitado,Monto_Financiado_Adjudicado,Tipo_Operacion) -- COMANDO A EJECUTAR
+VALUES 
+(NULL, NOW(), SESSION_USER(), NEW.Proyecto_Id,NEW.Moneda_Id,NEW.Monto_Solicitado,NEW.Monto_Adjudicado,NEW.Monto_Financiado_Solicitado,NEW.Monto_Financiado_Adjudicado,'Se inserto un nuevo registro en Financiamiento');
+
+
+-- Prueba: Inserto un valor en la tabla Financiamiento para verificar el funcionamiento del trigger.
+
+INSERT INTO Financiamiento
+VALUES
+(24654, 1, 5000000, 65423654, 500000, 500000);
