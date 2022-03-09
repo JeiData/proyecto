f93f63a67c40e3c7cf25898a857120ca636e733d
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-08 22:33:02 -0300

    Update funciones.sql

--- funciones.sql
+++ funciones.sql
@@ -1,6 +1,7 @@
+-- PRIMERA FUNCION
 
 CREATE FUNCTION `porcentaje_miembros_mujeres`(param1 INT , param2 INT) RETURNS int(11)
-NO SQL
+DETERMINATE
 BEGIN
 	DECLARE resultado INT;
  	DECLARE total_miembros INT;
@@ -9,7 +10,19 @@ BEGIN
 	RETURN resultado;
 END
 
+
+-- Estoy trabajando con Proyectos de Ciencia y Tecnología financiados por el Estado Nacional, la utilidad de esta función sería poder conocer la composición 
+-- de mujeres en cada proyecto. Un modo de usarla sería:
+
+USE proyectos_ciencia;
+
+SELECT Proyecto_Id, porcentaje_miembros_mujeres (Cantidad_Miembros_F,Cantidad_miembros_M) 
+FROM integrantes ;
+
+
 -----------------
+
+-- SEGUNDA FUNCION
 
 CREATE FUNCTION `FINANCIAMIENTO`(MONEDA_id INT) RETURNS varchar(30) READS SQL 
 DATA 
