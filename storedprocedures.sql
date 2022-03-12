769b64f2e5747edb91f01eb7af82cf53ca4983fc
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-12 20:40:31 -0300

    Update storedprocedures.sql

--- storedprocedures.sql
+++ storedprocedures.sql
@@ -1,35 +1,20 @@
 
--- El primer S.P. debe permitir indicar a través de un parámetro el campo de ordenamiento de una tabla y 
--- mediante un segundo parámetro, si el orden es descendente o ascendente:
+-- 1: (consigna) S.P. debe permitir indicar a través de un parámetro el campo de ordenamiento de una tabla y mediante un segundo parámetro, si el orden es descendente o ascendente.
+-- Pensé un Stored Procedure para optimizar las consultas en la tabla "Financiamiento". Permitiría seleccionar una determinada columna y realizar un ordenamiento ASC o DESC de
+-- de los diferentes montos.
 
-
-DELIMITER //
-CREATE PROCEDURE sp_ordenar_tabla (OUT PARAM1 VARCHAR (350))
+CREATE PROCEDURE `sp_ordenar_financiamiento`(IN MONTO VARCHAR (20), IN ASC_DESC VARCHAR (10))
 BEGIN
-	SELECT * FROM proyectos_2019
-    ORDER BY PARAM 1 DESC;
+	IF ASC_DESC='ASC' THEN
+		SELECT * FROM proyectos_ciencia.financiamiento
+		ORDER BY MONTO ASC;
+    ELSEIF ASC_DESC='DESC' THEN
+		SELECT * FROM proyectos_ciencia.financiamiento
+		ORDER BY MONTO DESC;
+END IF;
 END
 
-DELIMITER //
-
-
-
-
-
-DELIMITER %%
-CREATE PROCEDURE `sp_int_fem`()
-BEGIN
-	SELECT Proyecto_Id, Cantidad_Miembros_F, Cantidad_miembros_M, Sexo_director FROM integrantes
-	ORDER BY Cantidad_Miembros_F DESC ;
-END 
-DELIMITER %%
-
--- PARA APLICARLO SE INVOCA:
-
-CALL sp_int_fem;
-
-
--- 2: S.P. que puede "insertar" registros en una tabla de tu proyecto o "eliminar" algún registro específico de una tabla de tu proyecto.
+-- 2: (consigna) S.P. que puede "insertar" registros en una tabla de tu proyecto o "eliminar" algún registro específico de una tabla de tu proyecto.
 -- Para insertar registros, pueden ser dos opciones de SP:
 
 -- 2.1:
