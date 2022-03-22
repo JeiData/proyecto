2861ab7931da269fe993641b9cbcfabaf75be263
JeiData <99595411+JeiData@users.noreply.github.com>
2022-03-22 00:32:31 -0300

    Update tcl.sql

--- tcl.sql
+++ tcl.sql
@@ -56,3 +56,31 @@ ROLLBACK;
 
 COMMIT;
 
+-- CONSIGNA N°2: En la segunda tabla, inserta ocho nuevos registros iniciando también una transacción. 
+-- Agrega un savepoint a posteriori de la inserción del registro #4 y otro savepoint a posteriori del registro #8
+-- Agrega en una línea comentada la sentencia de eliminación del savepoint de los primeros 4 registros insertados
+
+-- tabla REFERENCIA DISCIPLINA
+START TRANSACTION;
+INSERT INTO ref_disciplina VALUES (289, 6, 'arte', 'arte prehistorico');
+INSERT INTO ref_disciplina VALUES (290, 2, 'psicologia', 'redes sociales');
+INSERT INTO ref_disciplina VALUES (291, 3, 'tecnologia', 'robotica');
+INSERT INTO ref_disciplina VALUES (292, 1, 'ciencias sociales', 'redes sociales');
+SAVEPOINT NUEVA_DISCIPLINA_1_4;
+INSERT INTO ref_disciplina VALUES (293, 4, 'arte', 'arte contemporaneo');
+INSERT INTO ref_disciplina VALUES (294, 3, 'musica', 'historia de la musica siglo XV');
+INSERT INTO ref_disciplina VALUES (295, 1, 'agronomia', 'comunidades y ambiente');
+INSERT INTO ref_disciplina VALUES (296, 4, 'agronomia', 'pesticidas');
+INSERT INTO ref_disciplina VALUES (297, 5, 'derecho', 'internacional ambiental');
+SAVEPOINT NUEVA_DISCIPLINA_5_8;
+
+-- Se verifican los cambios:
+
+SELECT * FROM ref_disciplina; 
+
+-- Por un lado se verifica la incorporación masiva de registros. 
+-- Por el otro, en ACTION OUTPUT, los marcadores:  "NUEVA_DISCIPLINA_1_4" y "NUEVA_DISCIPLINA_5_8"
+
+-- Para eliminar los primeros cuatro registros insertados, usaré:
+
+ROLLBACK TO NUEVA_DISCIPLINA_1_4; 
